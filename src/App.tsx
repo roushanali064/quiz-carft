62157c6efdab11a43a3617601de02b5980fe3dcc
Roushan ali <roushanrhaman064@gmail.com>
2024-01-24 14:57:03 +0600

    init and redux setup [done]

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-tailwind/react"
+
+function App() {
+  
+  return (
+    <>
+      <h1>hello</h1>
+      <Button placeholder={''} color="red">button</Button>
+    </>
+  )
+}
+
+export default App
